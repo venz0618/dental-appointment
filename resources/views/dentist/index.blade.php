02c68172382e5f5adc2195972ad3187dd42e6e62
venz0618 <100183442+venz0618@users.noreply.github.com>
2023-03-10 08:26:25 +0800

    new

---
+++ resources/views/dentist/index.blade.php
@@ -0,0 +1,6 @@
+@extends ('layouts.app')
+
+@section('content')
+Dentist Dashboard
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,22 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::view('/','home.homepage');
+Route::view('aboutUs','home.aboutUs');
+Route::view('services','home.services');
+Route::view('admin/dashboard','admin.index');
+Route::view('user/dashboard','user.index');
+Route::view('dentist/dashboard','dentist.index');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
